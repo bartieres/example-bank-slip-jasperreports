72b90a296d86e877632def7dc9374c98df422019
andre.bartieres <andre.bartieres@gft.com>
2024-05-25 12:36:39 -0300

    Finaliza implementacao

--- src/main/java/br/com/bartieres/jasper/boleto/dto/Banco.java
+++ src/main/java/br/com/bartieres/jasper/boleto/dto/Banco.java
@@ -1,65 +1,28 @@
 package br.com.bartieres.jasper.boleto.dto;
 
-import java.io.Serializable;
-import java.net.URL;
+import lombok.Data;
 
-/**
- * <p>Interface que define métodos específicos ao funcionamento de cada banco para
- * gerar o código de barras e a linha digitável.</p>
- * 
- * 
- * <p>Infelizmente cada banco funciona de uma maneira diferente para gerar esses
- * valores, e as vezes até mudam de funcionamento.</p>
- * 
- * <p>Documentação que pode ser consultada:</p>
- * 
- * <ul>
- *   <li>http://www.macoratti.net/boleto.htm</li>
- *   <li>http://pt.wikipedia.org/wiki/Boleto_bancário</li>
- * </ul>
- * 
- * <p>Para testes rapidos: http://evandro.net/codigo_barras.html</p>
- * 
- * <p>Apesar de possuirmos diversos unit tests, sempre é bom ter precaução com
- * valores e testar alguns boletos, em especial se valores serão altos.</p>
- * 
- * @author Paulo Silveira
- * @author Cauê Guerra
- * 
- */
-public interface Banco extends Serializable {
+import java.awt.image.BufferedImage;
 
-	/**
-	 * Retorna o número desse banco, formatado com 3 dígitos
-	 * 
-	 * @return numero formatado
-	 */
-	String getNumeroFormatado();
+@Data
+public class Banco {
 
-	/**
-	 * Pega a URL com a imagem de um banco
-	 * 
-	 * @return logo do banco
-	 */
-	URL getImage();
+	protected BufferedImage image;
 
-	/**
-	 * Gera o código de barras para determinado boleto
-	 * @param boleto boleto
-	 * @return código de barras (texto)
-	 */
-	String geraCodigoDeBarrasPara(Boleto boleto);
+	private String numeroFormatadoComDigito = "111-1";
 
-	String getCodigoBeneficiarioFormatado(Beneficiario beneficiario);
+	private String numeroFormatado;
 
-	String getCarteiraFormatado(Beneficiario beneficiario);
+	private String geraCodigoDeBarrasPara;
 
-	String getNossoNumeroFormatado(Beneficiario beneficiario);
+	private String codigoBeneficiarioFormatado;
 
-	String getAgenciaECodigoBeneficiario(Beneficiario beneficiario);
+	private String carteiraFormatado;
 
-	String getNumeroFormatadoComDigito();
+	private String nossoNumeroFormatado;
 
-	String getNossoNumeroECodigoDocumento(Boleto boleto);
+	private String agenciaECodigoBeneficiario;
+
+	private String nossoNumeroECodigoDocumento;
 
 }
--- src/main/java/br/com/bartieres/jasper/boleto/report/ReportGenerator.java
+++ src/main/java/br/com/bartieres/jasper/boleto/report/ReportGenerator.java
@@ -1,76 +1,39 @@
 package br.com.bartieres.jasper.boleto.report;
 
 import br.com.bartieres.jasper.boleto.dto.Boleto;
-import br.com.bartieres.jasper.boleto.dto.BoletoReport;
-import net.sf.jasperreports.engine.JREmptyDataSource;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
-import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ReportGenerator {
 
-    public static Map<String, Object> createMapObjectToReport(BoletoReport report) {
-        var map = new HashMap<String, Object>();
-        //map.put("logo", getImage("logo.png"));
-        map.put("beneficiario.nomeBeneficiario", "Andre Bartieres");
-        map.put("qrCode", "00020101021226980014br.gov.bcb.pix2576api-h.developer.btgpactual.com/pc/p/v2/cobv/e97abbcd057a4d79ac8d8ff52492023b5204000053039865802BR5924LLZ SOLUCAO COBRANCA S.A6014BELO HORIZONTE61083010000062070503***63049FBA");
-        map.put("numeroFormatadoComDigito", "208-1");
-        //map.put("linhaDigitavel", report.getLinhaDigitavel());
-        map.put("dtVencimento", report.getDtVencimento());
-        map.put("codigoDeBarras", "20890001091000000049681050767508197380000010000");
-        map.put("linhaDigitavel", "20890001091000000049681050767508197380000010000");
-        map.put("boleto", report);
-        return map;
-    }
-
-    private static Object getImage(String s) {
-
-        Image image = null;
-        InputStream stream;
-        try {
-            var url = ReportGenerator.class.getClassLoader().getResource("images");
-            File initialFile = new File(url.getPath().concat("/").concat(s));
-            stream = new FileInputStream(initialFile);
-            byte[] bytesImage = stream.readAllBytes();
-            ImageIcon imageIcon = new ImageIcon(bytesImage);
-            image = imageIcon.getImage();
-            stream.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return  image;
-    }
-
     public static void createReportFile(Boleto report) {
 
-        String file = "/src/main/resources/jasper/boleto-default.jasper";
+        String file = "/src/main/resources/jasper/boleto.jasper";
 
         try {
             Path path = Paths.get("");
-            String jasperFilePath = path.toAbsolutePath().toString() + file;
+            String jasperFilePath = path.toAbsolutePath() + file;
 
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(Arrays.asList(report));
             var jasperPrint = JasperFillManager.fillReport(jasperFilePath, null, dataSource);
 
-            //JasperPrint jasperPrint = JasperFillManager.fillReport(jasperFilePath, mapReport, new JREmptyDataSource());
             byte[] reportByte = JasperExportManager.exportReportToPdf(jasperPrint);
 
-            writeBytesToFileNio("/src/main/resources/relatorios/Relatorio1.pdf", reportByte);
+            writeBytesToFileNio("/src/main/resources/relatorios/Relatorio.pdf", reportByte);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -78,8 +41,30 @@ public class ReportGenerator {
 
     private static void writeBytesToFileNio(String s, byte[] report) throws IOException {
         Path path = Paths.get("");
-        String jasperFilePath = path.toAbsolutePath().toString() + s;
+        String jasperFilePath = path.toAbsolutePath() + s;
         Path path2 = Paths.get(jasperFilePath);
         Files.write(path2, report);
+    }
+
+    public static BufferedImage generateQRCodeImage(String text, int width, int height) {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            return MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getLocalizedMessage());
+        }
+    }
+
+    public static BufferedImage loadImage(String path) {
+        try (InputStream inputStream = ReportGenerator.class.getResourceAsStream(path)) {
+            if (inputStream == null) {
+                throw new IOException("Recurso não encontrado: " + path);
+            }
+            return ImageIO.read(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
